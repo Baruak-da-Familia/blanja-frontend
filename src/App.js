fb4473a62bd9bdf19abb2a7745aaec74df906edb
ganawa <ghana.afani@gmail.com>
2020-10-10 14:33:55 +0700

    [add] router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from './pages/Home';
 
 function App() {
-	return <></>;
+	return (
+		<>
+			<Router>
+				<Switch>
+					<Route path="/" exact component={Home} />
+				</Switch>
+			</Router>
+		</>
+	);
 }
 
 export default App;
