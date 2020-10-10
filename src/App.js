3c1f00d4b29377e2855a17a65f72d3d28af83c4e
ganawa <ghana.afani@gmail.com>
2020-10-10 13:08:37 +0700

    [update] global colors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styles from './assets/colors.module.css';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={styles.error}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
--- src/assets/colorStyles.js
+++
@@ -1,8 +0,0 @@
-export const black = '#222222';
-export const gray = '#9B9B9B';
-export const lightGray = '#D4D4D4';
-export const white = '##FFFFFF';
-export const lightWhite = '#F9F9F9';
-export const error = '#F01F0E';
-export const primary = '#DB3022';
-export const success = '#2AA952';
