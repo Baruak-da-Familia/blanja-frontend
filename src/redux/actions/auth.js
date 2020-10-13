464e7e46f2c7398b98e0aaa891a1a3680e521b2e
tavvfiq <taufiq.w@mail.ugm.ac.id>
2020-10-13 10:27:13 +0700

    merge

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -1,27 +1,32 @@
-import * as actions from './actionTypes';
-import { authLoginCustomer, authLoginSeller, authRegisterCustomer, authRegisterSeller  } from "../../utlis/reqData";
+import * as actions from "./actionTypes";
+import {
+	authLoginCustomer,
+	authLoginSeller,
+	authRegisterCustomer,
+	authRegisterSeller,
+} from "../../utils/reqData";
 
 export const authLoginCustomerCreator = (data) => {
-    return {
-      type: actions.AUTH_LOGIN_CUSTOMER,
-      payload: authLoginCustomer(data),
-    }
-  }
-  export const authLoginSellerCreator = (data) => {
-    return {
-      type: actions.AUTH_LOGIN_SELLER,
-      payload: authLoginSeller(data),
-    }
-  }
+	return {
+		type: actions.AUTH_LOGIN_CUSTOMER,
+		payload: authLoginCustomer(data),
+	};
+};
+export const authLoginSellerCreator = (data) => {
+	return {
+		type: actions.AUTH_LOGIN_SELLER,
+		payload: authLoginSeller(data),
+	};
+};
 export const authRegisterCustomerCreator = (data) => {
-  return {
-    type: actions.AUTH_REGISTER_CUSTOMER,
-    payload: authRegisterCustomer(data),
-  }
-}
+	return {
+		type: actions.AUTH_REGISTER_CUSTOMER,
+		payload: authRegisterCustomer(data),
+	};
+};
 export const authRegisterSellerCreator = (data) => {
-    return {
-      type: actions.AUTH_REGISTER_SELLER,
-      payload: authRegisterSeller(data),
-    }
-  }
+	return {
+		type: actions.AUTH_REGISTER_SELLER,
+		payload: authRegisterSeller(data),
+	};
+};
