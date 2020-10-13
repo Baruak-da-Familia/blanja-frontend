1941de30acb060ba76d6cb8431f75bcd572eb145
ganawa <ghana.afani@gmail.com>
2020-10-13 17:52:32 +0700

    [update] checkbox

--- src/pages/MyBag/MyBag.jsx
+++ src/pages/MyBag/MyBag.jsx
@@ -12,7 +12,7 @@ const MyBag = () => {
          seller: "Zalora Cloth",
          quantity: 1,
          price: 20.0,
-         selected: true,
+         selected: false,
          img: "https://s3-alpha-sig.figma.com/img/464a/22c1/4934cf1d9102bfc8ca226895c16fe510?Expires=1603065600&Signature=L2Go8ufnFXRu499YQ0SVJEFU8cW1i62rws4oM3PBc-WW3sCqbVw0AWsTnmqAMhltn5TMjdbR3EQjYS1QtRoZLSkt2Mh-AEfzKwMThJEAMb7oAI5dw1nCy1PVoEp9LQeco~tzGD5SJ9h8OzJgkoVGQ0YY1soJMVaC472GJxxHVZDfVctr2MEsi6EaHG-SqeNBVNHCcKM8EVDVhlTRT36AqDLeOSD10qWLtwInozO-8QW1w6hsZ2TmGRRXq4WjOaDU~8gLUUnxINBWB4m-FOwMs2DGjhpkQZQHe3B1fu0gIbL84W50DSX9X-w4PvTsTNFSgxsImCcSNvUX6Hsx5AavAg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
       },
       {
@@ -21,22 +21,35 @@ const MyBag = () => {
          seller: "Zalora Cloth",
          quantity: 1,
          price: 20.0,
-         selected: true,
+         selected: false,
          img: "https://s3-alpha-sig.figma.com/img/d373/227e/1b077d067cc7eed45f8733fd75f5e570?Expires=1603065600&Signature=d40ckFqK4v2u2r8FCajf9MOpyiz7NQhmacmlyXyfsLXRPWu-5MQ8RndW9wtuCOVRc~kbqsGaqwrfcjB4AgWVrtxYHETotH8XBuP5~rKUpUYxq1jUSWz5fo2WcHZvYKWFaF05tyYRfWOQWF7JB-q~69HXXeWfK6S~KA4wHmlMEVwMY66Q6nSvHxAqrXejnpENTskeO1Bp5zeypr~kd7N8c5oWsC8UQUV0M6ff1hcyhjT2YbgIDcAp6Y3fOXdKH4Iefow8ChLuq~jnAAfvE8Y8JWAXwZ713jAVTUpYVV2gpP-f-IZWypkWsoVUSB~GSmuYZjQX6xZ8c98qewV0Qj38Ow__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
       },
    ]);
 
-
    const handleSelectAll = (evt) => {
-
+      if (evt.target.checked) {
+         cart.map(item => item.selected = true);
+         setCart([...cart])
+      }
+      else {
+         cart.map(item => item.selected = false);
+         setCart([...cart])
+      }
    };
 
    const handleSelectItem = (evt) => {
-      console.log(evt.target);
+      if (evt.target.checked) {
+         let penampung = cart.filter(item => item.id === Number(evt.target.id));
+         penampung[0].selected = true;
+         setCart([...cart])
+      } else {
+         let penampung = cart.filter(item => item.id === Number(evt.target.id));
+         penampung[0].selected = false;
+         setCart([...cart])
+      }
    };
 
-   const handleIncrease = () => {
-      console.log(cart);
+   const handleIncrease = (evt) => {
    };
 
    return (
@@ -50,22 +63,32 @@ const MyBag = () => {
 
                <div className="row no-gutters shadow align-content-center container-select-all">
                   <div className="col-1 align-self-center">
-                     <input type="checkbox" name="selectAll" />
+                     <input
+                        type="checkbox"
+                        name="selectAll"
+                        onChange={handleSelectAll}
+                     />
                   </div>
                   <div className="col">
                      <p className={classname(text.text, "text-title")}>{`Select all item (${cart.length} items selected)`}</p>
                   </div>
                   <div className="col-1">
-                     <a href className={classname(text.text, colors.errorText, "text-title")}>Delete</a>
+                     <a href="#" className={classname(text.text, colors.errorText, "text-title")}>Delete</a>
                   </div>
                </div>
 
                {/* list item */}
                {cart.map(item => {
                   return (
-                     <div className="row no-gutters shadow align-items-center container-items">
+                     <div className="row no-gutters shadow align-items-center container-items" key={item.id}>
                         <div className="col-1 align-self-center">
-                           <input type="checkbox" name="items" onChange={handleSelectItem} />
+                           <input
+                              type="checkbox"
+                              name={item.name}
+                              onChange={handleSelectItem}
+                              id={item.id}
+                              checked={item.selected}
+                           />
                         </div>
                         <div className="col-2">
                            <img src={item.img} alt="" />
@@ -82,7 +105,7 @@ const MyBag = () => {
                            </div>
                         </div>
                         <div className="col-2">
-                           <p href className={classname(text.text, colors.blackText, "text-title text-right")}>
+                           <p href="#" className={classname(text.text, colors.blackText, "text-title text-right")}>
                               {`$ ${item.price.toFixed(1)}`}
                            </p>
                         </div>
