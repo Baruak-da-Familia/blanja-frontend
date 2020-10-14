0e13b9d1d1259b99875741f67218e296206d909a
tavvfiq <taufiq.w@mail.ugm.ac.id>
2020-10-14 15:06:10 +0700

    [update] add router with navbar

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -3,8 +3,8 @@ import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 // import { Button, FormGroup, FormControl } from "react-bootstrap"
 import {
-    authLoginCustomerCreator,
-    authLoginSellerCreator,
+	authLoginCustomerCreator,
+	authLoginSellerCreator,
 } from "../../redux/actions/auth";
 import corpName from "../../assets/img/logo.png";
 import classname from "../../helpers/classJoiner";
@@ -12,269 +12,270 @@ import styles from "./styles.module.css";
 import { useDispatch, useSelector } from "react-redux";
 
 const Login = () => {
-    const dispatch = useDispatch();
-    const [userType, setUserType] = useState(false);
-    const [errMsg, setErrMsg] = useState(null)
-    // const [errMsgSllr, setErrMsgSllr] = useState(null)
+	const dispatch = useDispatch();
+	const [userType, setUserType] = useState(false);
+	const [errMsg, setErrMsg] = useState(null);
+	// const [errMsgSllr, setErrMsgSllr] = useState(null)
 
-    const login = useSelector((state) => state.auth.user);
-    const statusLogin = useSelector((state) => state.auth.status);
-    const errMsgUser = useSelector((state) => state.auth.errMsg)
+	const login = useSelector((state) => state.auth.user);
+	const statusLogin = useSelector((state) => state.auth.status);
+	const errMsgUser = useSelector((state) => state.auth.errMsg);
 
-    const { handleSubmit, register, errors } = useForm();
+	const { handleSubmit, register, errors } = useForm();
 
-    useEffect(() => {
-        // console.log(statusLogin, login.user_type, errMsgUser)
-        
-        if (statusLogin === 200 && login.user_type === 'Customer') {
-            setErrMsg(null)
-            return console.log("customer dah login");
-        } else if (statusLogin === 200 && login.user_type === 'Seller') {
-            setErrMsg(null)
-            return console.log("seller dah login");
-        } else  {
-            setErrMsg(errMsgUser)
-            // console.log('kambing')
-        }
+	useEffect(() => {
+		// console.log(statusLogin, login.user_type, errMsgUser)
 
-    }, [statusLogin, login.user_type]);
+		if (statusLogin === 200 && login.user_type === "Customer") {
+			setErrMsg(null);
+			return console.log("customer dah login");
+		} else if (statusLogin === 200 && login.user_type === "Seller") {
+			setErrMsg(null);
+			return console.log("seller dah login");
+		} else {
+			setErrMsg(errMsgUser);
+			// console.log('kambing')
+		}
+	}, [statusLogin, login.user_type]);
 
+	const onSubmitCustomer = (data) => {
+		dispatch(authLoginCustomerCreator(data));
+		// console.log('customer')
+	};
+	const onSubmitSeller = (data) => {
+		dispatch(authLoginSellerCreator(data));
+		// console.log('seller')
+	};
 
-
-    const onSubmitCustomer = (data) => {
-        dispatch(authLoginCustomerCreator(data));
-        // console.log('customer')
-    };
-    const onSubmitSeller = (data) => {
-        dispatch(authLoginSellerCreator(data));
-        // console.log('seller')
-    };
-
-    return (
-        <div className={classname(styles.body)}>
-            {/* <p>FORM YANG INI PUNYA CUSTOMER</p> */}
-            {userType === false ? (
-                <div>
-                    <form
-                        className={classname(styles.login)}
-                        onSubmit={handleSubmit(onSubmitCustomer)}
-                    >
-                        <img
-                            alt="logo"
-                            className={classname(styles.logo)}
-                            src={corpName}
-                        />
-                        <p className={classname(styles.desc)}>
-                            Please login with your account
+	return (
+		<div className={classname(styles.body)}>
+			{/* <p>FORM YANG INI PUNYA CUSTOMER</p> */}
+			{userType === false ? (
+				<div>
+					<form
+						className={classname(styles.login)}
+						onSubmit={handleSubmit(onSubmitCustomer)}
+					>
+						<img
+							alt="logo"
+							className={classname(styles.logo)}
+							src={corpName}
+						/>
+						<p className={classname(styles.desc)}>
+							Please login with your account
 						</p>
 
-                        <div className={classname(styles.userType)}>
-                        {errMsg === null ? null : (<p className={classname(styles.errMsg)}>{errMsg}</p>)}
+						<div className={classname(styles.userType)}>
+							{errMsg === null ? null : (
+								<p className={classname(styles.errMsg)}>
+									{errMsg}
+								</p>
+							)}
 
-                            {userType === false ? (
-                                <button
-                                    className={classname(
-                                        styles.userTypeBtnCustomerActive
-                                    )}
-                                >
-                                    Customer
-                                </button>
-                            ) : (
-                                    <button
-                                        className={classname(
-                                            styles.userTypeBtnCustomer
-                                        )}
-                                    >
-                                        Customer
-                                    </button>
-                                )}
-                            <button
-                                className={classname(styles.userTypeBtnSeller)}
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                    setUserType(true);
-                                }}
-                            >
-                                Seller
+							{userType === false ? (
+								<button
+									className={classname(
+										styles.userTypeBtnCustomerActive
+									)}
+								>
+									Customer
+								</button>
+							) : (
+								<button
+									className={classname(
+										styles.userTypeBtnCustomer
+									)}
+								>
+									Customer
+								</button>
+							)}
+							<button
+								className={classname(styles.userTypeBtnSeller)}
+								onClick={(e) => {
+									e.preventDefault();
+									setUserType(true);
+								}}
+							>
+								Seller
 							</button>
-                        </div>
+						</div>
 
-                        <form className={classname(styles.formContainer)}>
-                            <p className={classname(styles.errMsg)}>
-                                {errors.email && errors.email.message}
-                            </p>
-                            <div>
-                                <input
-                                    className={classname(styles.emailInput)}
-                                    placeholder="Email"
-                                    name="email"
-                                    ref={register({
-                                        required: "Required",
-                                        pattern: {
-                                            value: /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/,
-                                            message: "bukan email",
-                                        },
-                                    })}
-                                />
-                            </div>
-
-                            <p className={classname(styles.errMsg)}>
-                                {errors.password && errors.password.message}
-                            </p>
-                            <div>
-                                <input
-                                    className={classname(styles.passwordInput)}
-                                    placeholder="Password"
-                                    name="password"
-                                    ref={register({
-                                        required: "Required",
-                                        pattern: {
-                                            value: /^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{8,}$/,
-                                            message: "bukan password",
-                                        },
-                                        validate: (value) =>
-                                            value !== "admin" || "Nice try!",
-                                    })}
-                                />
-                            </div>
-                            <p className={classname(styles.forgot)}>
-                                Forgot password?
+						<form className={classname(styles.formContainer)}>
+							<p className={classname(styles.errMsg)}>
+								{errors.email && errors.email.message}
 							</p>
-                            <button
-                                className={classname(styles.loginSubmit)}
-                                type="submit"
-                            >
-                                Submit
+							<div>
+								<input
+									className={classname(styles.emailInput)}
+									placeholder="Email"
+									name="email"
+									ref={register({
+										required: "Required",
+										pattern: {
+											value: /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/,
+											message: "bukan email",
+										},
+									})}
+								/>
+							</div>
+
+							<p className={classname(styles.errMsg)}>
+								{errors.password && errors.password.message}
+							</p>
+							<div>
+								<input
+									className={classname(styles.passwordInput)}
+									placeholder="Password"
+									name="password"
+									ref={register({
+										required: "Required",
+										pattern: {
+											value: /^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{8,}$/,
+											message: "bukan password",
+										},
+										validate: (value) =>
+											value !== "admin" || "Nice try!",
+									})}
+								/>
+							</div>
+							<p className={classname(styles.forgot)}>
+								Forgot password?
+							</p>
+							<button
+								className={classname(styles.loginSubmit)}
+								type="submit"
+							>
+								Submit
 							</button>
-                        </form>
-                    </form>
-                    <div className={classname(styles.signUpBtn)}>
-                        <p>
-                            Don't have a Tokopedia account?{" "}
-                            <span onClick={() => { }}>
-                                <Link
-                                    className={classname(styles.bla)}
-                                    to="/Register"
-                                >
-                                    Register
+						</form>
+					</form>
+					<div className={classname(styles.signUpBtn)}>
+						<p>
+							Don't have a Tokopedia account?{" "}
+							<span onClick={() => {}}>
+								<Link
+									className={classname(styles.bla)}
+									to="/Register"
+								>
+									Register
 								</Link>
-                            </span>
-                        </p>
-                    </div>
-                </div>
-            ) : (
-                    // <p>FORM DIBAWAH PUNYA SELLER, YANG ATAS PUNYA CUSTOMER</p>
-                    <div>
-                        <form
-                            className={classname(styles.login)}
-                            onSubmit={handleSubmit(onSubmitSeller)}
-                        >
-                            <img
-                                alt="logo"
-                                className={classname(styles.logo)}
-                                src={corpName}
-                            />
-                            <p className={classname(styles.desc)}>
-                                Please login with your seller account
+							</span>
+						</p>
+					</div>
+				</div>
+			) : (
+				// <p>FORM DIBAWAH PUNYA SELLER, YANG ATAS PUNYA CUSTOMER</p>
+				<div>
+					<form
+						className={classname(styles.login)}
+						onSubmit={handleSubmit(onSubmitSeller)}
+					>
+						<img
+							alt="logo"
+							className={classname(styles.logo)}
+							src={corpName}
+						/>
+						<p className={classname(styles.desc)}>
+							Please login with your seller account
 						</p>
 
-                            {/* {errMsgSllr === null ? null : (<p className={classname(styles.errMsg)}>{errMsgSllr}</p>)} */}
-                            <div className={classname(styles.userType)}>
-                                <button
-                                    className={classname(
-                                        styles.userTypeBtnCustomer
-                                    )}
-                                    onClick={(e) => {
-                                        e.preventDefault();
-                                        setUserType(false);
-                                    }}
-                                >
-                                    Customer
+						{/* {errMsgSllr === null ? null : (<p className={classname(styles.errMsg)}>{errMsgSllr}</p>)} */}
+						<div className={classname(styles.userType)}>
+							<button
+								className={classname(
+									styles.userTypeBtnCustomer
+								)}
+								onClick={(e) => {
+									e.preventDefault();
+									setUserType(false);
+								}}
+							>
+								Customer
 							</button>
-                                {userType === true ? (
-                                    <button
-                                        className={classname(
-                                            styles.userTypeBtnSellerActive
-                                        )}
-                                    >
-                                        Seller
-                                    </button>
-                                ) : (
-                                        <button
-                                            className={classname(
-                                                styles.userTypeBtnSeller
-                                            )}
-                                        >
-                                            Seller
-                                        </button>
-                                    )}
-                            </div>
+							{userType === true ? (
+								<button
+									className={classname(
+										styles.userTypeBtnSellerActive
+									)}
+								>
+									Seller
+								</button>
+							) : (
+								<button
+									className={classname(
+										styles.userTypeBtnSeller
+									)}
+								>
+									Seller
+								</button>
+							)}
+						</div>
 
-                            <form className={classname(styles.formContainer)}>
-                                <p className={classname(styles.errMsg)}>
-                                    {errors.email && errors.email.message}
-                                </p>
-                                <div>
-                                    <input
-                                        className={classname(styles.emailInput)}
-                                        placeholder="Email"
-                                        name="email"
-                                        ref={register({
-                                            required: "Required",
-                                            pattern: {
-                                                value: /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/,
-                                                message: "bukan email",
-                                            },
-                                        })}
-                                    />
-                                </div>
-
-                                <p className={classname(styles.errMsg)}>
-                                    {errors.password && errors.password.message}
-                                </p>
-                                <div>
-                                    <input
-                                        className={classname(styles.passwordInput)}
-                                        placeholder="Password"
-                                        name="password"
-                                        ref={register({
-                                            required: "Required",
-                                            pattern: {
-                                                value: /^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{8,}$/,
-                                                message: "bukan password",
-                                            },
-                                            // validate: value => value !== "admin" || "Nice try!"
-                                        })}
-                                    />
-                                </div>
-                                <p className={classname(styles.forgot)}>
-                                    Forgot password?
+						<form className={classname(styles.formContainer)}>
+							<p className={classname(styles.errMsg)}>
+								{errors.email && errors.email.message}
 							</p>
-                                <button
-                                    className={classname(styles.loginSubmit)}
-                                    type="submit"
-                                >
-                                    Submit
+							<div>
+								<input
+									className={classname(styles.emailInput)}
+									placeholder="Email"
+									name="email"
+									ref={register({
+										required: "Required",
+										pattern: {
+											value: /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/,
+											message: "bukan email",
+										},
+									})}
+								/>
+							</div>
+
+							<p className={classname(styles.errMsg)}>
+								{errors.password && errors.password.message}
+							</p>
+							<div>
+								<input
+									className={classname(styles.passwordInput)}
+									placeholder="Password"
+									name="password"
+									ref={register({
+										required: "Required",
+										pattern: {
+											value: /^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{8,}$/,
+											message: "bukan password",
+										},
+										// validate: value => value !== "admin" || "Nice try!"
+									})}
+								/>
+							</div>
+							<p className={classname(styles.forgot)}>
+								Forgot password?
+							</p>
+							<button
+								className={classname(styles.loginSubmit)}
+								type="submit"
+							>
+								Submit
 							</button>
-                            </form>
-                        </form>
-                        <div className={classname(styles.signUpBtn)}>
-                            <p>
-                                Don't have a Tokopedia account?{" "}
-                                <span onClick={() => { }}>
-                                    <Link
-                                        className={classname(styles.bla)}
-                                        to="/Register"
-                                    >
-                                        Register
+						</form>
+					</form>
+					<div className={classname(styles.signUpBtn)}>
+						<p>
+							Don't have a Tokopedia account?{" "}
+							<span onClick={() => {}}>
+								<Link
+									className={classname(styles.bla)}
+									to="/Register"
+								>
+									Register
 								</Link>
-                                </span>
-                            </p>
-                        </div>
-                    </div>
-                )}
-        </div>
-    );
+							</span>
+						</p>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default Login;
