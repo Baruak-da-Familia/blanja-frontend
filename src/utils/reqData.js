c97c59e27dc24a04574c5c27a57a348d6525b331
solehudin5699 <solehudinamqi@yahoo.co.id>
2020-10-15 10:40:49 +0700

    add functionality

--- src/utils/reqData.js
+++ src/utils/reqData.js
@@ -1,16 +1,43 @@
-import Axios from 'axios';
+import Axios from "axios";
 
 export const authLoginCustomer = (data) => {
-    return Axios.post(`http://localhost:8000/auth/login/customer`, data);
-}
+  return Axios.post(`http://localhost:8000/auth/login/customer`, data);
+};
 
 export const authLoginSeller = (data) => {
-    return Axios.post(`http://localhost:8000/auth/login/seller`, data);
-}
+  return Axios.post(`http://localhost:8000/auth/login/seller`, data);
+};
 
 export const authRegisterCustomer = (data) => {
-    return Axios.post(`http://localhost:8000/auth/register/customer`, data);
-}
+  return Axios.post(`http://localhost:8000/auth/register/customer`, data);
+};
 export const authRegisterSeller = (data) => {
-    return Axios.post(`http://localhost:8000/auth/register/seller`, data);
-}
+  return Axios.post(`http://localhost:8000/auth/register/seller`, data);
+};
+export const updateProfileCustomer = (id, body) => {
+  return Axios.patch(`http://localhost:8000/customer/${id}`, body, {
+    headers: {
+      "content-type": "multipart/form-data",
+      contentType: false,
+      mimeType: "multipart/form-data",
+      "cache-control": "no-cache",
+      accept: "application/json",
+    },
+  });
+};
+
+export const addAddressCustomer = (id, data) => {
+  return Axios.patch(`http://localhost:8000/customer-address/${id}`, data);
+};
+
+export const addProduct = (body) => {
+  return Axios.post(`http://localhost:8000/product`, body, {
+    headers: {
+      "content-type": "multipart/form-data",
+      contentType: false,
+      mimeType: "multipart/form-data",
+      "cache-control": "no-cache",
+      accept: "application/json",
+    },
+  });
+};
